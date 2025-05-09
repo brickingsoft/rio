d90c96c9c877cfe0f7b3aeff43897bc691ad0cd8
wangminxiang <ryougi.nevermore@hotmail.com>
2025-05-09 19:25:59 +0800

    fix posix

--- conn.go
+++ conn.go
@@ -1,7 +1,6 @@
 package rio
 
 import (
-	"github.com/brickingsoft/rio/pkg/liburing/aio"
 	"net"
 )
 
@@ -10,8 +9,6 @@ import (
 // Multiple goroutines may invoke methods on a Conn simultaneously.
 type Conn interface {
 	net.Conn
-	// Fd export aio net fd.
-	Fd() (*aio.NetFd, error)
 	// SetSocketOptInt set socket option, the func is limited to SOL_SOCKET level.
 	SetSocketOptInt(level int, optName int, optValue int) (err error)
 	// GetSocketOptInt get socket option, the func is limited to SOL_SOCKET level.
